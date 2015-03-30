8ec5c850111a97d16c363681bebbb2ed32d8b4ed
wladkom <testbox.84@mail.ru>
2015-03-30 22:00:23 +0300

    add Main.java to Task2

--- Task2/src/vreshetnyak/Main.java
+++ Task2/src/vreshetnyak/Main.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package vreshetnyak;
 
 /**
@@ -12,11 +6,22 @@ package vreshetnyak;
  */
 public class Main {
 
+    private static boolean[] color;
+    private static int[][] map;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        byte map[][] = {
+            {0, 0, 0, 0, 1, 1},
+            {0, 1, 1, 0, 0, 0},
+            {0, 1, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0},
+            {0, 0, 1, 1, 0, 0},};
+        
+
     }
-    
+
 }
